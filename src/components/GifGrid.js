81f5804ad5bde0797e9063af1a1b927096005b48
Carlos Londono <c.londono.m@gmail.com>
2022-05-10 21:14:43 -0500

    components files added

---
+++ src/components/GifGrid.js
@@ -0,0 +1,23 @@
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
+import "animate.css";
+
+export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFetchGifs(category);
+
+  return (
+    <>
+      <h3>{category}</h3>
+
+      {loading && (
+        <p className="card-grid animate__animated animate__flash">Loading...</p>
+      )}
+
+      <div className="card-grid animate__animated animate__fadeIn">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
